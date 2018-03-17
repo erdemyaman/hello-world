6e663bcfbe0fe980cde72737d1573118e18ed539
erdemyaman <yaman.erdem1@gmail.com>
2018-03-18 00:59:43 +0100

    minor

--- HelloWorld/node.h
+++ HelloWorld/node.h
@@ -9,9 +9,13 @@ public:
 	virtual ~Node();
 	
 	unsigned getId() {return m_id;}
+	void setData(int data) {m_data = data;}
+	
+	
 	void print(bool escape = true);
 	
 private:
 	unsigned m_id;
 	Node* m_parent;
+	int m_data;
 };
